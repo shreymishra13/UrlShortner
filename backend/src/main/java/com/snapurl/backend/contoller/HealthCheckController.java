8137cbefaecc0fa9ee4cf87c34c0a1ba1a827eca
Shrey <mishra.shrey001@gmail.com>
2025-12-27 15:58:15 +0530

    Added the Slf4j logger

--- backend/src/main/java/com/snapurl/backend/contoller/HealthCheckController.java
+++ backend/src/main/java/com/snapurl/backend/contoller/HealthCheckController.java
@@ -1,9 +1,12 @@
 package com.snapurl.backend.contoller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class HealthCheckController {
 
@@ -11,6 +14,7 @@ public class HealthCheckController {
     @GetMapping("/health-check")
     public String getHealthCheck(){
 
+        log.info("Inside the get health - check");
         return "Health Check Ok in get Request";
 
 
